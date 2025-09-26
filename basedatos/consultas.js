b4a4b7564fe82747cb96902c5622c8e1234b450c
Jesus Jara <jarajesus9558@gmail.com>
2025-09-26 18:53:00 -0500

    Add - descripcion product in detalle_producto.html, and add product info

--- basedatos/consultas.js
+++ basedatos/consultas.js
@@ -1,14 +1,34 @@
 function obtenerProductosServicios(categoria = "") {
     /*
     let lista_filtrada = [];
-    for (let int i = 0; i < lista_cursos.length; i++) {
-        if (lista_cursos[i].codigo_categoria === categoria) {
-            lista_filtrada.push(lista_cursos[i]);
+    for (let int i = 0; i < lista_productos.length; i++) {
+        if (lista_productos[i].codigo_categoria === categoria) {
+            lista_filtrada.push(lista_productos[i]);
         }
     }
     return lista_filtrada;
     */
- 
-    let lista_filtrada = lista_productos.filter(productos => productos.codigo_categoria === categoria);
-    return lista_filtrada;
+
+    // Si la categoria es vacia devolver todos los productos
+    if (categoria === null || categoria == " ") {
+        return lista_productos
+
+    } 
+    // Sino, Enviar la categoria que se solicite
+    else {
+        let lista_filtrada = lista_productos.filter(productos => productos.codigo_categoria === categoria);
+        return lista_filtrada;
+    }
+}
+
+function obtenerProductoServicioPorCodigo(codigo) {
+    /*
+    for (let index = 0; lista_productos < lista_productos.length; index++) {
+        if( lista_productos[index].cidigo === codigo ) {
+            return lista_productos[index];
+        }
+    }
+    return null;
+    */
+    return lista_productos.find(productos => productos.codigo === codigo);    
 }
